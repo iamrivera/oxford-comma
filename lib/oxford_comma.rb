ca732cdbe3e4c7761b4412bf7f78cf70b69120fb
iamrivera <a3m3rivera@gmail.com>
2020-10-08 15:16:24 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+    if array.length > 2
+      new_end = "and " + array.pop
+      array << new_end
+      array.join(", ")
+    elsif array.length == 2
+       array.join(" and ")
+    else 
+      array.join()
+    end
+  end
